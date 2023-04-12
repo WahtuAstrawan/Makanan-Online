b41bd460ea784afb121cc0acb0934623d43eec21
Wahtu Astrawan <chatt699@gmail.com>
2023-04-12 17:49:48 +0800

    Memperbaiki konsep login

--- src/Login.java
+++ src/Login.java
@@ -1,70 +1,43 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.Console;
 
 public class Login {
-    public static void login() {
+    public static int login() {
+        Fitur.clearScreen();
+        Console console = System.console();
         Scanner input = new Scanner(System.in);
-        String idCostumer = "pelanggan", pwCostumer = "mamat123", idAdmin = "admin", pwAdmin = "wahtuGanteng";
-        while (true) {
-            System.out.println("Selamat Datang di Program Restoran ! ");
-            System.out.println("1. Login ");
-            System.out.println("2. Keluar ");
-            System.out.print("Pilihan anda : ");
-            String masukkan = input.nextLine();
-            if (masukkan.matches("\\d+")) {
-                int pilih = Integer.parseInt(masukkan);
-                if (pilih == 1) {
-                    while (true) {
-                        clearScreen();
-                        System.out.println("\t Login Page");
-                        System.out.print("Username : ");
-                        String username = input.nextLine();
-                        // Console console = System.console();
-                        // char[] pass = console.readPassword("Password : ");
-                        // String password = new String(pass); "Hanya bisa pada terminal (membuat input password tidak terlihat)"
-                        System.out.print("Password : ");
-                        String password = input.nextLine();
-                        if (username.equals(idAdmin) && password.equals(pwAdmin)) {
-                            clearScreen();
-                            Admin adm = new Admin();
-                            adm.menuAdmin();
-                            break;
-                        } else if (username.equals(idCostumer) && password.equals(pwCostumer)) {
-                            clearScreen();
-                            Customer cstm = new Customer();
-                            cstm.menuCustomer();
-                            break;
-                        } else {
-                            System.out.println("Username atau Password yang Anda masukkan salah ! ");
-                            getChar();
-                        }
-                    }
-                    break;
-                } else if (pilih == 2) {
-                    System.exit(0);
-                    break;
+        int pilih = Fitur.validasiInteger(
+                "Selamat Datang di Program Restoran Online ! \n1. Login \n2. Keluar \nPilihan anda : ",
+                "Maaf input anda salah, Pilih opsi yang tersedia !", 1, 2);
+        if (pilih == 1) {
+            while (true) {
+                Fitur.clearScreen();
+                System.out.println("\t Login Page");
+                System.out.print("Username : ");
+                String username = input.nextLine();
+                char[] pass = console.readPassword("Password : ");
+                String password = new String(pass);
+                if (username.equals(Admin.idAdmin) && password.equals(Admin.pwAdmin)) {
+                    Fitur.clearScreen();
+                    return 1;
+                } else if (username.equals(Customer.idCostumer) && password.equals(Customer.pwCostumer)) {
+                    Fitur.clearScreen();
+                    return 2;
                 } else {
-                    System.out.println("Maaf input Anda salah, Tolong masukkan angka yang tersedia ! ");
-                    getChar();
+                    int ulang = Fitur.validasiInteger(
+                            "\nUsername atau Password yang Anda masukkan salah ! \nMasukkan 1 untuk ulang input, atau 2 untuk kembali ke menu awal \nPilihan anda : ",
+                            "Maaf input anda salah, Pilih opsi yang tersedia !",
+                            1, 2);
+                    if (ulang == 1) {
+                        continue;
+                    } else if (ulang == 2) {
+                        login();
+                    }
                 }
-            } else {
-                System.out.println("Maaf input Anda salah, Tolong masukkan angka ! ");
-                getChar();
-                clearScreen();
             }
+        } else if (pilih == 2) {
+            System.exit(0);
         }
-    }
-
-    public static void clearScreen() {
-        for (int i = 0; i < 50; i++) {
-            System.out.println("");
-        }
-    }
-
-    public static void getChar() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("   Tekan enter untuk mengulang ! ");
-        String enter = input.nextLine();
+        return 0;
     }
 }

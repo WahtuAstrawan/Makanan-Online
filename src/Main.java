549900958df097073ad6505c705164b002ba617f
Wahtu Astrawan <chatt699@gmail.com>
2023-04-12 17:50:05 +0800

    Memperbaiki konsep main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-class Main {
+import java.util.ArrayList;
+
+public class Main {
+    // ======================================
     public static void main(String[] args) {
-        Login.login();
+        ArrayList<Restoran> daftarRestoran = new ArrayList<Restoran>();
+        int ulang = 0;
+        do {
+            int jenisUser = Login.login();
+            if (jenisUser == 1) {
+                ulang = Admin.menuAdmin(daftarRestoran);
+            } else if (jenisUser == 2) {
+                ulang = Customer.menuCustomer(daftarRestoran);
+            }
+        } while (ulang == 1);
     }
 }
-

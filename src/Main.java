a1bc0487bbc372747397289dbc93307440f02a9e
Wahtu Astrawan <chatt699@gmail.com>
2023-04-14 20:47:52 +0800

    Menambahkan array list class pesanan untuk menu customer

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class Main {
-    // ======================================
     public static void main(String[] args) {
         ArrayList<Restoran> daftarRestoran = new ArrayList<Restoran>();
+        ArrayList<Pesanan> daftarPesanan = new ArrayList<Pesanan>();
         int ulang = 0;
         do {
             int jenisUser = Login.login();
             if (jenisUser == 1) {
                 ulang = Admin.menuAdmin(daftarRestoran);
             } else if (jenisUser == 2) {
-                ulang = Customer.menuCustomer(daftarRestoran);
+                ulang = Customer.menuCustomer(daftarRestoran, daftarPesanan);
             }
         } while (ulang == 1);
     }
